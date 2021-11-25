423e56c4fd9f0a66c096e38807764d478c70e3d2
Baltej Randhawa <baltej.randhawa0@gmail.com>
2021-11-24 18:05:22 -0800

    fished the hover to change color part

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ for(let i = 0 ; i<16*16-1; i++){
     pixle.setAttribute('class','pixle')
     container.appendChild(pixle); 
 }
+
+// const squares = document.querySelectorAll('.pixle');
+// squares.forEach(square =>{
+//     square.addEventListener('mouseover',myFunc(e))
+// })
+
+const square = document.querySelector('.pixle');
+square.addEventListener('mouseover',()=>{
+    square.classList.toggle('visited');
+})
+
+const squares = document.querySelectorAll('.pixle');
+squares.forEach(square =>{
+    square.addEventListener('mouseover',() =>{
+        square.classList.toggle('visited');
+    })
+})
