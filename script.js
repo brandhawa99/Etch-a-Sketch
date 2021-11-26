6bbbc5daddf860836f405fca39e4e6bd077b9987
Baltej Randhawa <baltej.randhawa0@gmail.com>
2021-11-25 16:56:36 -0800

    fixed boxes

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('.button');
+const value_display = document.querySelector('.value-display')
+const slider = document.querySelector('.slider');
+
 const SIZE = 600;
 let num = 30;
+
+value_display.innerHTML = slider.value;
+
+slider.oninput = function(){
+    let num = this.value;
+    value_display.innerHTML = this.value;
+}
+
+function makeSquares(){
+    for(let i = 0 ; i<num*num; i++){
+        let pixle = document.createElement('div');
+        pixle.setAttribute('class','pixle')
+        container.appendChild(pixle); 
+    }
+    
+    
+    const squares = document.querySelectorAll('.pixle');
+    squares.forEach(square =>{
+        square.addEventListener('mouseover',() =>{
+            square.classList.toggle('visited');
+        })
+    })
+
+}
+
+
+
 
 for(let i = 0 ; i<num*num; i++){
     let pixle = document.createElement('div');
