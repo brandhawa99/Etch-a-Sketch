120c7ae471d7fdbf12e0f8b5a6dcf4b9ab6c0538
Baltej Randhawa <baltej.randhawa0@gmail.com>
2021-11-25 18:34:41 -0800

    finsihed product

--- script.js
+++ script.js
@@ -2,53 +2,51 @@ const container = document.querySelector('.container');
 const button = document.querySelector('.button');
 const value_display = document.querySelector('.value-display')
 const slider = document.querySelector('.slider');
+const body = document.querySelector('body');
 
-const SIZE = 600;
-let num = 30;
+const BOARD_WIDTH_HEIGHT = 600;
 
 value_display.innerHTML = slider.value;
 
 slider.oninput = function(){
-    let num = this.value;
     value_display.innerHTML = this.value;
+    removeChildren(container);
+    makeBoard(this.value)
 }
 
-function makeSquares(){
-    for(let i = 0 ; i<num*num; i++){
-        let pixle = document.createElement('div');
-        pixle.setAttribute('class','pixle')
-        container.appendChild(pixle); 
+body.onload = function(){
+    makeBoard(16);
+}
+
+function makeBoard(num_of_squares){
+    let pixle_size = Math.round((BOARD_WIDTH_HEIGHT/num_of_squares)*10)/10;
+    for(let i = 0; i<num_of_squares*num_of_squares;i++){
+        const pixle = document.createElement('div');
+        pixle.className = 'pixle'
+        pixle.setAttribute('style',`width:${pixle_size}px; height: ${pixle_size}px`)
+        container.appendChild(pixle);
     }
-    
-    
-    const squares = document.querySelectorAll('.pixle');
+
+    let squares = document.querySelectorAll('.pixle');
     squares.forEach(square =>{
         square.addEventListener('mouseover',() =>{
             square.classList.toggle('visited');
         })
     })
-
 }
 
-
-
-
-for(let i = 0 ; i<num*num; i++){
-    let pixle = document.createElement('div');
-    pixle.setAttribute('class','pixle')
-    container.appendChild(pixle); 
+function removeChildren(parent){
+    while(parent.lastChild){
+        parent.removeChild(parent.lastChild);
+    }
 }
-
-
-const squares = document.querySelectorAll('.pixle');
-squares.forEach(square =>{
-    square.addEventListener('mouseover',() =>{
-        square.classList.toggle('visited');
-    })
-})
 
 button.addEventListener('click',() =>{
+    let squares = document.querySelectorAll('.pixle');
     squares.forEach(square =>{
-        square.classList.remove('visited');
+            square.classList.remove('visited');
     })
 })
+
+
+
